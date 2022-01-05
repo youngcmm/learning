9ef72b2bd8a53c8b233ae36a4b45654994f49934
youngcmm <96936962+youngcmm@users.noreply.github.com>
2022-01-05 14:50:59 +0800

    Create month1_4_00.cpp

---
+++ c_cpp快速入门/month1_4_00.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    //2.3选择语句if
+         //if (){}else if(){}else{}
+         //2.3.2if嵌套
+         //2.3.3switch语句
+         //switch(a){
+         //case 1:
+            //break;
+            //注意switch判断a之后只是从case中寻找到入口并逐一向后执行，若不加break则之后的case不论真假都会执行。
+            //default: 若全部case为假则执行defaul。
+
+}
+
+
+
