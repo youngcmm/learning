2d26856529874afdd17fac99c2ea7330435a2b42
youngcmm <96936962+youngcmm@users.noreply.github.com>
2022-01-09 12:38:04 +0800

    Create month1_5_00.cpp

---
+++ c_cpp快速入门/month1_5_00.cpp
@@ -0,0 +1,40 @@
+    //2.4循环结构
+        //2.4.1 while语句，
+        //2.4.2do...while语句，
+        //do
+        //{
+        //}while();
+        //2.4.3for语句for(;;){};for的小括号内部条件分格是用的分号。
+        //2.4.4break 和 continue
+        //continue结束该次循环。
+#include <stdio.h>
+int main()
+{
+        //int sum = 0 ;
+        //for(int i = 1; i <= 5 ;i++){
+        //    if(i % 2 == 1) continue;
+        //        sum = sum + i;
+        //    }
+        //printf("%d",sum);
+    //2.5 数组
+        //2.5.1一维数组
+        //2.5.2冒泡排序!!!!!!!!!!!!!
+//        int a[10]={3,1,4,5,2};
+//        int temp = 0;
+//        for(int i = 1; i<5; i++)
+//        {
+//            for(int j = 0; j < 5 - i; j++ )
+//            {
+//                if(a[j] >= a[j+1])
+//                {
+//                    temp = a[j];
+//                    a[j] = a[j+1];
+//                    a[j+1] = temp;
+//                }
+//            }
+//        }
+//        for(int i = 0;i < 5; i++)
+//        printf("%d",a[i]);
+        //2.5.3二维数组
+return 0;
+}
